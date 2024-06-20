5b144c4e4bcc37ae49e5476c8a9a2e40cee822f1
Dana <mitsunokehl@gmail.com>
2024-06-20 19:50:59 +0300

    add mongo db

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import contactsRouter from './routes/contactsRouter.js';
-
 dotenv.config();
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
-
 
 app.use((_, res) => {
   res.status(404).json({ message: 'Route not found' });
