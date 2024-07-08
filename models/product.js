21f4ed322756362bfd734ce979dd88572ffa3283
Dana <mitsunokehl@gmail.com>
2024-07-08 14:58:41 +0300

    fix product model

--- models/product.js
+++ models/product.js
@@ -1,14 +1,26 @@
 import { Schema, model } from 'mongoose';
 
+const categories = [
+  'Medicine',
+  'Head',
+  'Hand',
+  'Dental Care',
+  'Skin Care',
+  'Eye Care',
+  'Vitamins & Supplements',
+  'Orthopedic Products',
+  'Baby Care',
+];
+
 const productSchema = new Schema(
   {
     photo: { type: String },
     name: { type: String },
-    address: { type: String },
+    suppliers: { type: String },
     products: { type: Number },
+    stock: { type: Number },
     price: { type: Number },
-    status: { type: String },
-    order_date: { type: Date },
+    category: { type: String, enum: categories },
   },
   { versionKey: false, timestamps: false }
 );
